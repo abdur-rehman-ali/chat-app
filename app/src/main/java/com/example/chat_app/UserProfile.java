9531794ffe8405b26a46cbe03c7ac7d1dd952f1d
abdur-rehman-ali <alijoyia911@gmail.com>
2021-06-07 22:45:15 +0500

    Profile image added using picasso

--- app/src/main/java/com/example/chat_app/UserProfile.java
+++ app/src/main/java/com/example/chat_app/UserProfile.java
@@ -2,16 +2,20 @@ package com.example.chat_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.squareup.picasso.Picasso;
 
 public class UserProfile extends AppCompatActivity {
 
     TextView username, email;
+    ImageView profileImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,14 +24,18 @@ public class UserProfile extends AppCompatActivity {
         //Targeting user
         username = findViewById(R.id.username);
         email = findViewById(R.id.email);
+        profileImage = findViewById(R.id.profileImage);
 
         FirebaseUser currentuser = FirebaseAuth.getInstance().getCurrentUser();
         if(currentuser!=null)
         {
             String current_username = currentuser.getDisplayName();
             String current_email = currentuser.getEmail();
+            Uri image_url = currentuser.getPhotoUrl();
             username.setText(current_username);
             email.setText(current_email);
+            Picasso.with(getApplicationContext()).load(image_url).into(profileImage);
+
         }
     }
 }
