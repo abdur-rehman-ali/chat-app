96a2a58dd454def05d62b153d452f98f8af83afb
abdur-rehman-ali <alijoyia911@gmail.com>
2021-06-07 13:10:09 +0500

    sign out implemented

--- app/src/main/java/com/example/chat_app/MainActivity.java
+++ app/src/main/java/com/example/chat_app/MainActivity.java
@@ -1,14 +1,69 @@
 package com.example.chat_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView name,email;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name=findViewById(R.id.name);
+        email=findViewById(R.id.email);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            // User is signed in
+            String user_name = user.getDisplayName();
+            String user_email = user.getEmail();
+            name.setText(user_name);
+            email.setText(user_email);
+
+        } else {
+            // No user is signed in
+        }
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu,menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.sign_out:
+                signOut();
+                break;
+            default:
+                break;
+        }
+
+        return true;
+    }
+
+    private void signOut() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(MainActivity.this,SignInActivity.class);
+        startActivity(intent);
+    }
+
 }
