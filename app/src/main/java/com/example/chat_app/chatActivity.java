d9dd207cd17ef7e52341507a500259495acecc1f
abdur-rehman-ali <alijoyia911@gmail.com>
2021-06-11 20:17:52 +0500

    message sending functionality implemented

--- app/src/main/java/com/example/chat_app/chatActivity.java
+++ app/src/main/java/com/example/chat_app/chatActivity.java
@@ -1,7 +1,10 @@
 package com.example.chat_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
@@ -12,7 +15,10 @@ import com.example.chat_app.Adapters.MessageAdapter;
 import com.example.chat_app.Model.Messges;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -26,6 +32,7 @@ public class chatActivity extends AppCompatActivity {
     Button sendButton;
     EditText messageBox;
     FirebaseDatabase database;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,8 +50,12 @@ public class chatActivity extends AppCompatActivity {
         senderRoom = senderUid+receiverUid;
         receiverRoom  = receiverUid+senderUid;
 
+        recyclerView=findViewById(R.id.recycler_view_chat_activity);
 
         database = FirebaseDatabase.getInstance();
+        getMessages();
+
+
         sendButton = findViewById(R.id.sendMessage);
         messageBox = findViewById(R.id.messageBox);
         sendButton.setOnClickListener(new View.OnClickListener() {
@@ -54,12 +65,13 @@ public class chatActivity extends AppCompatActivity {
 
                 Date date = new Date();
                 Messges messageDetails = new Messges(message,senderUid,date.getTime());
+                messageBox.setText("");
 
-                database.getReference().child("chats").child(senderRoom).child("message")
+                database.getReference().child("chats").child(senderRoom).child("message").push()
                         .setValue(messageDetails).addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void aVoid) {
-                        database.getReference().child("chats").child(receiverRoom).child("message")
+                        database.getReference().child("chats").child(receiverRoom).child("message").push()
                                 .setValue(messageDetails).addOnSuccessListener(new OnSuccessListener<Void>() {
                             @Override
                             public void onSuccess(Void aVoid) {
@@ -76,6 +88,32 @@ public class chatActivity extends AppCompatActivity {
 
         messages=new ArrayList<>();
         adapter=new MessageAdapter(this,messages);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+
+    }
+
+    private void getMessages() {
+        database.getReference().child("chats")
+                .child(senderRoom)
+                .child("message")
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        messages.clear();
+                        for (DataSnapshot postSnapshot: snapshot.getChildren()) {
+
+                            Messges msg= postSnapshot.getValue(Messges.class);
+                            messages.add(msg);
+                        }
+                        adapter.notifyDataSetChanged();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
     }
 
     @Override
