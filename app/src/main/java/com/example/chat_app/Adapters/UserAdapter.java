951e44a944bf7d9b36a6149b6a53420e63db334b
abdur-rehman-ali <alijoyia911@gmail.com>
2021-06-11 17:26:34 +0500

    Fetched users from database and displayed them in chatlist

---
+++ app/src/main/java/com/example/chat_app/Adapters/UserAdapter.java
@@ -0,0 +1,65 @@
+package com.example.chat_app.Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.chat_app.Model.UserProfileData;
+import com.example.chat_app.R;
+import com.example.chat_app.UserProfile;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder> {
+
+    Context context;
+    ArrayList<UserProfileData> usersData;
+    private TextView title;
+
+    public UserAdapter(Context context, ArrayList<UserProfileData> usersData) {
+        this.context = context;
+        this.usersData = usersData;
+    }
+
+    @NonNull
+    @Override
+    public UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.main_screen_list_item_layout,parent,false);
+        return new UserViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserViewHolder holder, int position) {
+     UserProfileData user= usersData.get(position);
+    holder.title.setText(user.getName());
+    holder.subtitle.setText(user.getEmail());
+    Picasso.with(context).load(user.getUri()).into(holder.icon);
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return usersData.size();
+    }
+
+    public class UserViewHolder extends RecyclerView.ViewHolder
+    {
+        TextView title ;
+        TextView subtitle;
+        ImageView icon;
+        public UserViewHolder(@NonNull View itemView) {
+            super(itemView);
+            title=itemView.findViewById(R.id.title);
+            subtitle=itemView.findViewById(R.id.subtitle);
+            icon = itemView.findViewById(R.id.icon);
+        }
+    }
+}
--- app/src/main/java/com/example/chat_app/MainActivity.java
+++ app/src/main/java/com/example/chat_app/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -14,6 +15,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.chat_app.Adapters.UserAdapter;
 import com.example.chat_app.Model.UserProfileData;
 import com.example.chat_app.Model.users;
 import com.example.chat_app.Model.usersAdapter;
@@ -23,6 +25,10 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -36,7 +42,10 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView name,email;
     FirebaseFirestore db;
-
+    FirebaseDatabase database;
+    ArrayList<UserProfileData> users;
+    UserAdapter userAdapter;
+    RecyclerView userslist;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,8 +53,14 @@ public class MainActivity extends AppCompatActivity {
 
 
         db=FirebaseFirestore.getInstance();
+        database = FirebaseDatabase.getInstance();
+        users = new ArrayList<>();
+        userAdapter=new UserAdapter(this,users);
 
-        displayUsers();
+        userslist=findViewById(R.id.recycler_view);
+        userslist.setAdapter(userAdapter);
+
+        displayData();
 
 
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -53,56 +68,28 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void displayUsers() {
-        ListView listView = findViewById(R.id.list);
-        ArrayList<users> usersList = new ArrayList<users>();
-        usersList.add(new users("Abdur Rehman Ali","alijoyia911@gmail.com","asKk1MzoXdUdoVUx8D7Eptb9aCm1","https://lh3.googleusercontent.com/a-/AOh14Gjj7FmaByauPgwPFkvs8zy2QjDiqPM3SbJMfo5G=s96-c"));
+    private void displayData() {
 
+        database.getReference().child("users").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot postSnapshot: snapshot.getChildren()) {
+                    UserProfileData user = postSnapshot.getValue(UserProfileData.class);
+                    users.add(user);
+                }
+                userAdapter.notifyDataSetChanged();
+            }
 
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
 
-        //Display users in log
-//        db.collection("chat_users").addSnapshotListener(new EventListener<QuerySnapshot>() {
-//            @Override
-//            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
-//
-//                if(error!=null)
-//                {
-//                    return;
-//                }
-//
-//
-//
-//                for(QueryDocumentSnapshot doc:value)
-//                {
-//
-//                    usersList.add(new users(doc.get("name").toString(),doc.get("email").toString(),doc.get("uuid").toString(),doc.get("uri").toString()));
-//
-//                }
-//
-//            }
-//        });
-
-        db.collection("chat_users")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                Log.d("data", document.getId() + " => " + document.getData().get("email"));
-                                usersList.add(new users(document.getData().get("name").toString(),document.getData().get("email").toString(),document.getData().get("uuid").toString(),document.getData().get("uri").toString()));
-                            }
-
-
-                        } else {
-//                            Log.d(TAG, "Error getting documents: ", task.getException());
-                        }
-                    }
-                });
-        usersAdapter mUserAdapter = new usersAdapter(this,usersList);
-        listView.setAdapter(mUserAdapter);
-
+            }
+        });
     }
+
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -141,6 +128,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
 
-        displayUsers();
     }
 }
